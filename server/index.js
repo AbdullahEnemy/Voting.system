c32aa7f472005f4e9b7ce26553deb11b1ca85ff6
AbdullahEnemy <akbuddy8@gmail.com>
2023-08-14 04:35:44 +0500

    MongoDb Setup Config

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express();
+const mongoose= require('mongoose')
+const PORT =4000;
+mongoose.connect("mongodb://127.0.0.1:27017/local")
+.then(()=>{
+    console.log('connected')
+})
+.catch((err)=>{
+
+    console.log('error')
+})
 app.get("/", (req,res)=>{
     res.send("Hello Enemy");
 });
-app.listen(4000,()=>{
+app.listen(PORT,()=>{
     console.log("serve started listening to app")
 
 });

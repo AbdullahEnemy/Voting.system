91b0a893cd97b3f5c942f1d8ab6d721e16ed1084
AbdullahEnemy <akbuddy8@gmail.com>
2023-08-14 05:51:41 +0500

    user model defined

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
 const mongoose= require('mongoose')
+let  user=require('./models/user')
 const PORT =4000;
 mongoose.connect("mongodb://127.0.0.1:27017/local")
 .then(()=>{
@@ -17,3 +18,25 @@ app.listen(PORT,()=>{
     console.log("serve started listening to app")
 
 });
+/*let user1=new user({
+
+    email:"test1@exmaple.com",
+    username:"enemy1",
+    password:"12345678",
+    userType:"admin",
+    CNIC:"1234567829",
+    constituency:"greentown"
+})
+
+
+
+user1.save();*/
+
+user.find().then((u)=>{
+    console.log(u)
+})
+.catch(()=>
+console.log("Error:user not found")
+)
+
+

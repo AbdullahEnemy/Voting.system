2df5346b965300dcb2cf240786c104c5b11fca27
AbdullahEnemy <127130143+AbdullahEnemy@users.noreply.github.com>
2023-08-14 16:40:58 +0500

    user login implemented (#7)

--- server/controllers/users_controllers.js
+++ server/controllers/users_controllers.js
@@ -1,5 +1,5 @@
 const User = require("../models/user");
-
+const bcrypt = require("bcryptjs");
 const signup = async (req, res) => {
   try {
     await User.create(req.body);
@@ -8,7 +8,23 @@ const signup = async (req, res) => {
     res.status(500).json({ error: `An error occurred during signup: ${error}` });
   }
 };
-
+const login = async (req, res) => {
+    try {
+      const user = await User.findOne({ email: req.body.email });
+      if (user) {
+        const auth = await bcrypt.compare(req.body.password, user.password)
+        if (!auth) {
+          return res.status(401).json({ message: "Password is incorrect" });
+        }
+        res.status(200).json({ message: "Login successful", user:user });
+      } else {
+        res.status(400).json({ message: "User not found." });
+      }
+    } catch (error) {
+      res.status(500).json({ error: `An error occurred during login: ${error}` });
+    }
+  };
+  
 
 const userController = {
   signup: signup,
--- server/routes/users_router.js
+++ server/routes/users_router.js
@@ -5,7 +5,7 @@ const signupController = require('../controllers/users_controllers');
 
 const router = express.Router()
 
-router.post('/signup', signupController.signup)
-
+router.post('/signup', signupController.signup);
+router.get('/login', signupController.login)
 
 module.exports = router

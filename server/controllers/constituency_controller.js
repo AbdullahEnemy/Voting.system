820aa77acb3fdfa1331c2e6921b731a84129e2c1
AbdullahEnemy <akbuddy8@gmail.com>
2023-08-14 22:35:16 +0500

    CRUD operations implemented

--- server/controllers/constituency_controller.js
+++ server/controllers/constituency_controller.js
@@ -2,10 +2,11 @@ const Constituency = require("../models/constituency");
 
 module.exports.register = async (req, res) => {
   try {
-    console.log("Condition: ", !req.user && (req.user.userType !== 'admin'))
-    if (req.user && (req.user.userType !== 'admin')) return res.json({ status: 403, message: "You are not allowed to perform this action"});
+    if (authurize_user(req, res)) return res;
 
-    const existingConstituency = await Constituency.findOne({ number: req.body.number });
+    const existingConstituency = await Constituency.findOne({
+      number: req.body.number,
+    });
     if (existingConstituency) {
       return res.json({ message: "Constituency already exists" });
     }
@@ -14,8 +15,92 @@ module.exports.register = async (req, res) => {
 
     res.status(200).json({ message: "Constituency registered successfully" });
   } catch (error) {
-    res
-      .status(500)
-      .json({ error: `An error occurred during constituency registeration: ${error}` });
+    res.status(500).json({
+      error: `An error occurred during constituency registeration: ${error}`,
+    });
   }
-}
+};
+
+module.exports.index = async (req, res) => {
+  try {
+    if (authurize_user(req, res)) return res;
+
+    const constituencies = await Constituency.find();
+    if (!constituencies) {
+      return res.json({ message: "No Constituency Registered" });
+    }
+
+    res.status(200).json(constituencies);
+  } catch (error) {
+    res.status(500).json({
+      error: `An error occurred when fetching constituencies: ${error}`,
+    });
+  }
+};
+
+module.exports.delete = async (req, res) => {
+  try {
+    if (authurize_user(req, res)) return res;
+
+    const deletedConstituency = await Constituency.findOneAndDelete({
+      _id: req.params.id,
+    });
+
+    if (!deletedConstituency) {
+      return res.status(400).json({ message: "Constituency not found" });
+    }
+
+    res.status(200).json({ message: "Constituency deleted successfully" });
+  } catch (error) {
+    res.status(500).json({
+      error: `An error occurred during constituency deletion: ${error}`,
+    });
+  }
+};
+
+module.exports.update = async (req, res) => {
+  try {
+    if (authurize_user(req, res)) return res;
+
+    const constituency = await Constituency.findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: { district: req.body.district } }
+    );
+
+    if (!constituency) {
+      return res.status(400).json({ message: "Constituency not found" });
+    }
+
+    res.status(200).json({ message: "Constituency Updated successfully" });
+  } catch (error) {
+    res.status(500).json({
+      error: `An error occurred during constituency updation: ${error}`,
+    });
+  }
+};
+
+module.exports.show = async (req, res) => {
+  try {
+    if (authurize_user(req, res)) return res;
+
+    const constituency = await Constituency.findOne({ _id: req.params.id });
+
+    if (!constituency) {
+      return res.json({ message: "No Constituency Found" });
+    }
+
+    res.status(200).json(constituency);
+  } catch (error) {
+    res.status(500).json({
+      error: `An error occurred during constituency fetching: ${error}`,
+    });
+  }
+};
+
+const authurize_user = (req, res) => {
+  if (req.user && req.user.userType !== "admin")
+    return res.json({
+      status: 403,
+      message: "You are not allowed to perform this action",
+    });
+};
--- server/routes/constituency_router.js
+++ server/routes/constituency_router.js
@@ -5,5 +5,10 @@ const { userVerification } = require("../middlewares/auth_middleware");
 const router = express.Router();
 
 router.post("/register", userVerification, constituencyController.register);
+router.get("/", userVerification, constituencyController.index);
+router.delete("/:id", userVerification, constituencyController.delete);
+router.patch("/:id", userVerification, constituencyController.update);
+router.get("/:id", userVerification, constituencyController.show);
+
 
 module.exports = router;

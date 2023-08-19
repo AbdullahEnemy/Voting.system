85602c4d7f9bdafe0799be62573ce943f8287bb9
AbdullahEnemy <127130143+AbdullahEnemy@users.noreply.github.com>
2023-08-20 04:35:24 +0500

    Revert "Request crud (#16)"
    This reverts commit fd94b1e22988cdbf57ce70a938e07d83ee1541ba.

--- server/routes/vote_router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const voteController = require("../controllers/vote_controller");
-const { userVerification } = require("../middlewares/auth_middleware");
-const router = express.Router();
-router.post("/register", userVerification, voteController.register);
-router.get("/", userVerification, voteController.index);
-router.get("/count", userVerification, voteController.countVotes);
-
-module.exports = router;
